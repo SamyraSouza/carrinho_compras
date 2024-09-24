5db0946f740c768b05ccba412d987880b4435e6a
Samyra Souza <167902992+SamyraSouza@users.noreply.github.com>
2024-09-24 12:19:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,12 @@ header("Access-Control-Allow-Methods: POST, GET");
 
 require './vendor/autoload.php';
 
+try {
 print_r((new App\User())->all());
+} catch (\Exception $e) {
+  echo $e->getMessage();
+}
+  
 
 include './src/routes/api.php';
 include './src/routes/web.php';
