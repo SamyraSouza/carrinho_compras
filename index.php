502bc1dbf837d4d33da8e5995d184a3a85a4cd03
Samyra Souza <167902992+SamyraSouza@users.noreply.github.com>
2024-09-24 12:24:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,5 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET");
 
 require './vendor/autoload.php';
-
-try {
-$user = new App\User();
-$user->initConnection();
-print_r($user->all());
-$user->closeConnection();
-} catch (\Exception $e) {
-  echo $e->getMessage();
-}
-  
-
 include './src/routes/api.php';
 include './src/routes/web.php';
