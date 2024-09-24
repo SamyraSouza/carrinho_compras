9a4915d9e24e8138dfa4a7702873e3908d1e761f
Samyra Souza <167902992+SamyraSouza@users.noreply.github.com>
2024-09-24 12:45:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 date_default_timezone_set("America/Sao_Paulo");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require './vendor/autoload.php';
 include './src/routes/api.php';
